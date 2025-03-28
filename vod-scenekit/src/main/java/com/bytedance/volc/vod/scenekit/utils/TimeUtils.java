5d871d2996a3efc5a0c693974c6fd0ce13011fe1
zhangfan.frank <zhangfan.frank@bytedance.com>
2025-03-28 14:27:48 +0800

    others: opt time format

--- vod-scenekit/src/main/java/com/bytedance/volc/vod/scenekit/utils/TimeUtils.java
+++ vod-scenekit/src/main/java/com/bytedance/volc/vod/scenekit/utils/TimeUtils.java
@@ -18,41 +18,39 @@
 
 package com.bytedance.volc.vod.scenekit.utils;
 
-import java.util.Formatter;
-import java.util.Locale;
-
-
 public class TimeUtils {
-
-    private static Formatter sFormatter;
-    private static StringBuilder sFormatBuilder;
 
     /**
      * 格式化时间 timeMS -> HH:MM:SS
-     *
-     * @param timeMs 根据短视频的应用场景，使用int
      */
     public static String time2String(long timeMs) {
         if (timeMs < 0) {
-            return ""; //or throw an exception
+            return "";
         }
-
         long totalSeconds = timeMs / 1000;
 
         long seconds = totalSeconds % 60;
         long minutes = (totalSeconds / 60) % 60;
         long hours = totalSeconds / 3600;
 
-        if (sFormatter == null) {
-            sFormatBuilder = new StringBuilder();
-            sFormatter = new Formatter(sFormatBuilder, Locale.getDefault());
+        StringBuilder timeString = new StringBuilder();
+        if (hours > 0) {
+            if (hours < 10) {
+                timeString.append('0');
+            }
+            timeString.append(hours).append(':');
         }
 
-        sFormatBuilder.setLength(0);
-        if (hours > 0) {
-            return sFormatter.format("%02d:%02d:%02d", hours, minutes, seconds).toString();
-        } else {
-            return sFormatter.format("%02d:%02d", minutes, seconds).toString();
+        if (minutes < 10) {
+            timeString.append('0');
         }
+        timeString.append(minutes).append(':');
+
+        if (seconds < 10) {
+            timeString.append('0');
+        }
+        timeString.append(seconds);
+
+        return timeString.toString();
     }
 }
